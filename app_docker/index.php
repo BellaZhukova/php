b968ffed5851316db4ccaec40b9a06f253eecae5
BellaZhukova <izabellaef390@gmail.com>
2024-04-04 12:58:06 +0700

    composer json

--- app_docker/index.php
+++
@@ -1,1 +0,0 @@
-<?php echo ' lksdcojdsncjdns'; phpinfo();
---
+++ pop-it-mvc/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-directory-and-file-structure',
+        'version' => 'dev-directory-and-file-structure',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'fb13b18e71eeeb5c5268dd960a98a20c95407176',
+        'name' => 'izabella/pop-it-mvc',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'izabella/pop-it-mvc' => array(
+            'pretty_version' => 'dev-directory-and-file-structure',
+            'version' => 'dev-directory-and-file-structure',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'fb13b18e71eeeb5c5268dd960a98a20c95407176',
+            'dev_requirement' => false,
+        ),
+    ),
+);
